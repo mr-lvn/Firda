bf889950bff57a44f5d630ad170c7cd0a21d7a36
CallMeVinn <vinndeveloper@gmail.com>
2025-01-25 23:18:01 +0700

    update eval.js

--- commands/Developer/eval.js
+++ commands/Developer/eval.js
@@ -1,9 +1,42 @@
 const { CommandBuilder } = require("../../lib/");
 
+const { EmbedBuilder } = require("discord.js");
+const util = require("node:util");
+
 module.exports = new CommandBuilder({
-    description: "",
+    description: "Code evaluation tool",
     
-    execute(ci) {
+    async execute(ci) {
+        if (!ci.args.length) return;
         
+        const code = ci.args.join(" ");
+        
+        const embed = new EmbedBuilder().setColor(ci.client.config.color);
+
+        try {
+
+            embed.setTitle("Results")
+            embed.setDescription(`\`\`\`js\n${clean(await eval(code))}\`\`\``);
+
+            ci.reply({ content: null, embeds: [embed] });
+        }
+        catch(error) {
+            embed.setColor("Red");
+            embed.setTitle("Results")
+            embed.setDescription(`\`\`\`js\n${clean(error)}\`\`\``);
+
+            ci.reply({ content: null, embeds: [embed] });
+        }
     }
 })
+
+function clean(code) {
+    if  (typeof code === "string") {
+        return code
+            .replace(/`/g, "`" + String.fromCharCode(8203))
+            .replace(/@/g, "@" + String.fromCharCode(8203));
+    }
+    else {
+        return util.inspect(code, { depth: 0 });
+    }
+}
