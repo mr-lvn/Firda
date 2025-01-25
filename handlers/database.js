c8bff9718b2c59c56457a4ea30c02ede128f7f6f
CallMeVinn <vinndeveloper@gmail.com>
2025-01-26 02:33:02 +0700

    update database.js

--- handlers/database.js
+++ handlers/database.js
@@ -10,10 +10,9 @@ module.exports = async(client) => {
     
     const mySqlDriver = new MySQLDriver({
         host: process.env.MYSQL_HOST,
-        port: 18645,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: "data"
+        database: "defaultdb"
     });
     
     await mongoDriver.connect();
