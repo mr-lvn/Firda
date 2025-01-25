cea181f20dc6f36856a02a05f2934efa5410b56a
CallMeVinn <vinndeveloper@gmail.com>
2025-01-25 23:18:04 +0700

    update config.js

--- root/config.js
+++ root/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     color: "#FFBA00",
     developerId: "561170896480501790",
-    prefix: "fi.",
+    prefix: "ly.",
     link: {
         discord: "https://discord.gg",
         website: "https://firda.web.app"
