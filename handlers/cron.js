3c55ba78b4a64a1d66f13f2409126d52fa7e0335
CallMeVinn <vinndeveloper@gmail.com>
2025-01-27 01:40:33 +0700

    update cron.js

--- handlers/cron.js
+++ handlers/cron.js
@@ -1,8 +1,8 @@
 const cron = require("node-cron");
 
 module.exports = () => {
-    cron.schedule('4 0 0 1 * * *', () => {
-        console.log('Running a job at 01:40 at Asia/Jakarta timezone');
+    cron.schedule('4 5 0 1 * * *', () => {
+        console.log('Running a job at 01:45 at Asia/Jakarta timezone');
     }, {
         scheduled: true,
         timezone: "Asia/Jakarta"
