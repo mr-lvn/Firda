f4ea41c787f7ef797fba12b758acff31a84e3761
CallMeVinn <vinndeveloper@gmail.com>
2025-01-26 22:08:41 +0700

    update database.js

--- handlers/database.js
+++ handlers/database.js
@@ -10,9 +10,35 @@ module.exports = async(client) => {
     
     const mySqlDriver = new MySQLDriver({
         host: process.env.MYSQL_HOST,
+        port: 18645,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: "defaultdb"
+        database: "defaultdb",
+        ssl: `-----BEGIN CERTIFICATE-----
+MIIEQTCCAqmgAwIBAgIUV8S1EMkyFY+YhL8V7cahmscm/PUwDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvMzJlYTU4MzItODhiMS00ODgxLWI1MDMtNDhkZDZhYjFh
+ZDlhIFByb2plY3QgQ0EwHhcNMjUwMTI1MTA1NDU5WhcNMzUwMTIzMTA1NDU5WjA6
+MTgwNgYDVQQDDC8zMmVhNTgzMi04OGIxLTQ4ODEtYjUwMy00OGRkNmFiMWFkOWEg
+UHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAKAoyXAb
+wOqqHHbyxaFKVfRE9AQfhQfn/7qNuWmu9a5arhxGcmaGYWUojq+u2gM1UcsyN228
+I3ND1+jFztDJKm6F2YF8MprMOkVNDIsG2WcCQEE8TkaFMaEV0DgRkxkKVXi/gmNd
+VSvR1ZtGZrVeKLXfhz8NVlZvtzWPZwhBheKJA0C22U9guAL0RZyp5f0crzUCpWtc
+05FRM/ziwJP/J8p7HpR3Xf+Bvt8GQ9ahi3DCmq8xdnTNPdBVlFqB1fqIsdP84HiK
+T52jQs9ldGcozyYqlIqgBuGqxU9XTs9TUxwJVNpbsSp1GWGfvF+JAU1rYOERlE07
+hF+cSA3tuCK4/iwbv999/xIiVfleB+6IvCufVrHtpTw9Mjcmsy5eVE1yD6gj5Wtj
+haNAI/PrpcrETcWQkzYemuebwi792FnFP0mNDR/kPeW97TRc0M3+L4cO5Hr3b1wG
+l5YLAh9SuF/56wL03JhYkiFXhvnExOSw1GiJXzu2z/iKeFHCVdnsmV4+KwIDAQAB
+oz8wPTAdBgNVHQ4EFgQUP0lyPjPt0+F1cbkcXgsjdDoFHq4wDwYDVR0TBAgwBgEB
+/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBAJYrqCLkBa6A8eXU
+8kXZVa+h6elLXuZr6BMDrSVPlvDjDYQwxCfJX16mW+lrTLsKC01QvmlXa6EGQRrB
+XzbClM6VGPBXv31qgbtNy4HKGYn6IX+3edBJ882SMxYhkZRlegvOUNw9PgPKoUj6
+lG+dEzoGOpaWWPndNsofGdRPUfv2joEdL13+fq8Hdi+tpYaw36UP2iDgRNFJ/8Hg
+wmS2qlEy6eXKQCQBB3FJWTZgZjaoL6zyNK8mTaEgs/36asSKPNSIKqJlw0JYRnMB
+7zi6uqFRcvdGhcIEs5Z8ZKlwbPsiScN3vMlHlNLdBxI85eidwai0+znNxu6RcGut
+9/osTLmupgInr5Te/JAlAtS8lqb4ajvnxMKD8MZ3qCbTUGyMti2As0TC5pe6n7kD
+RxVRhhBQlBRjsHW1PBovVXJYh+x9R1y1dVp0SxweLsCeb59ziSCnYXw1nngfQDaf
+QIcsW8gOKmo0/j5OT1mJ4bQs9km1wkFlbPVEy0lFjEbes52y2A==
+-----END CERTIFICATE-----`
     });
     
     await mongoDriver.connect();
