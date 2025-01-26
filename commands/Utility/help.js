a2d54ecbcb36f975b272b83404a959bc088525d9
CallMeVinn <vinndeveloper@gmail.com>
2025-01-27 03:25:51 +0700

    update help.js

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -15,6 +15,9 @@ module.exports = new CommandBuilder({
         for (const category of categories) {
             embed.addFields({ name: category, value: ci.client.commands.filter(cmds => cmds.category === category).map(cmd => `\`${cmd.name}\``).join(", "), inline: true })
         }
+        embed.setAuthor({ name: "Commands List", iconURL: message.guild.iconURL({ size: 256, forceStatic: true }) })
+        .setDescription(`This is a list of commands. If you want specific help with a command use \`${ci.prefix}help [command name]\` or more help info join our [discord server](${ci.config.link.discord})`)
+        .setThumbnail(ci.client.user.displayAvatarURL({ size: 512, forceStatic: true }));
         
         ci.reply({ embeds: [embed] });
     }
