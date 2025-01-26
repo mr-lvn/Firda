4bfcfd16e52af6d6ddcb1dad18cc942daeda6a9b
CallMeVinn <vinndeveloper@gmail.com>
2025-01-26 23:33:51 +0700

    update

---
+++ commands/Social/rank.js
@@ -0,0 +1,33 @@
+const { CommandBuilder } = require("../../lib/");
+
+const { getUser } = require("../../functions/");
+
+module.exports = new CommandBuilder({
+    description: "",
+    
+    async execute(ci) {
+        const user = await getUser(ci);
+        const member = ci.guild.members.cache.get(user.id);
+        
+        const currentXp = ci.db.get(member.user.id+"_Xp");
+        const currentLevel = ci.db.get(member.user.id+"_Level");
+        
+        const card = new RankCardBuilder()
+            .setUsername(cutName(member.user.username))
+            .setDisplayName(cutName(member.user.displayName || member.nickname || ""))
+            .setAvatar(member.user.displayAvatarURL({ size: 1024, extension: "png" }))
+            .setCurrentXP(currentXp)
+            .setRequiredXP(currentXp*currentLevel*3)
+            .setLevel(currentLevel)
+            .setRank(1)
+            .setStatus("online");
+        
+        const image = await card.build({ format: 'png' });
+        
+        ci.reply({ files: [{ attachment: image, name: member.user.username+"_Rank.png" }] });
+    }
+})
+
+function cutName(name) {
+    return name.length > 10 ? name.substr(0, 10)+"..." : name;
+}
---
+++ functions/getUser.js
@@ -0,0 +1,19 @@
+module.exports = async (ci) => {
+    const query = !ci.args.length ? ci.args.join(" ") : ci.args.join(" ").toLowerCase();
+    return ci.ctx.mentions.first() ?? await findUser(ci, query) ?? ci.user; 
+}
+
+async function findUser(ci, query) {
+    const users = ci.client.users;
+    const members = ci.guild.members;
+    
+    if (isNaN(query)) {
+        const user = users.cache.find(u => u.username.toLowerCase() == query || u.globalName.toLowerCase() == query);
+        const member = ci.guild.members.cache.find(m => m.nickname.toLowerCase() == query);
+        if (user) return user;
+        else return member ? member.user : void 0;
+    }
+    else {
+        return users.cache.get(query) ?? await users.fetch(query).catch(_=>{});
+    }
+}
